259c88eccb6a2c72e7fd3e6340046b28a0a2af36
jyoti-wp <sim_s006@icloud.com>
2021-07-12 17:01:10 +0530

    verify.php update

--- custom-function/verify.php
+++ custom-function/verify.php
@@ -23,3 +23,32 @@ function send_verification_email($user_email, $verification_code)
 
     return mail($to, $subject, $message);
 }
+
+/**
+ * Verify user and update subscription.
+ *
+ * @param $user_email
+ * @param $verification_code
+ * @param $mysqli
+ *
+ * @return array
+ */
+
+function verify_user_and_update_subscription($user_email, $verification_code, $mysqli)
+{
+    $subscribed  = 'yes';
+    $is_verified = 'yes';
+    $response    = [
+        'success'       => false,
+        'error_message' => '',
+    ];
+
+    $does_user_exists = does_user_exist($user_email, $mysqli);
+
+    if (!$does_user_exists) {
+        $response['success']       = false;
+        $response['error_message'] = 'user does not exist';
+
+        return $response;
+    }
+}
--- verify.php
+++ verify.php
@@ -7,6 +7,10 @@
 include __DIR__ . '/databaseconnection.php';
 include __DIR__ . '/custom-function/verify.php';
 
+$user_email = isset($_GET['email']) ? $_GET['email'] : '';
+$action = 'verify.php?email=' . $user_email;
+$verification_code = isset($_POST['verification_code']) ? $_POST['verification_code'] : '';
+
 ?>
 
 <!DOCTYPE html>
@@ -16,11 +20,23 @@ include __DIR__ . '/custom-function/verify.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Kindly verify</title>
 </head>
 
 <body>
-
+    <div class="container">
+        <a href="/index.php">Back to Home Page!</a>
+        <div class="row">
+            <div class="column form form-action">
+                <h3 class="text-center">Kindly check your email for verification code!</h3>
+                <form action="" method="POST">
+                    <input type="text" name="verification code" value="<?php echo $verification_code ?>">
+                    <input type="text" hidden name="email" value="<?php echo $user_email; ?>">
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
